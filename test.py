ccf704cb7682fc2fc0712fd1d79fdb831be11adf
John <ghitaghost@gmail.com>
2024-04-07 16:44:39 +0100

    Got somewhere, check test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ with open("grades.txt") as file:
     number = file.read()
     number = number.lower()
     for ch in "qwertyuiopasdfghjklzxcv\nbnm!#$%&\"()*+-./:;<=>?@[\\]^_`{|}~'":
-        number = number.replace(ch, ' ')
-
-    stripped_number = number.strip()
-    print(stripped_number)
+        number = number.replace(ch, '')
+    if ',' in number:
+        number = number.split(',')
+    print(number)
