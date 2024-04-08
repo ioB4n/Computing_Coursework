edb391fc41ed355c35bb545b8ca622a821329bf5
John <ghitaghost@gmail.com>
2024-04-08 11:40:42 +0100

    Read_from_file is working

--- read_from_file.py
+++ read_from_file.py
@@ -1,23 +1,28 @@
 def read_file(number_list):
-    num_list = number_list[:]
-    with open("grades.txt") as file:
-        content = file.read()
-        content = content.lower()
-        for ch in "qwertyuiopasdfghjklzxcv\nbnm! #$%&\"()*+-./:;<=>?@[\\]^_`{|}~'":
-            content = content.replace(ch, '')
-        if ',' in content:
-            numbers = content.split(',')
-            for num in numbers:
-                if num.isdigit():
-                    num = int(num)
-                    if num >= 0 and num <= 100:
-                        num_list.append(num)
-        else:
-            if content.isdigit():
-                content = int(content)
-                if content >= 0 and content <= 100:
-                    num_list.append(content)
-    return num_list
+    try:
+        num_list = number_list[:]
+        with open("grades.txt") as file:
+            content = file.read()
+            content = content.lower()
+            for ch in "qwertyuiopasdfghjklzxcv\nbnm! #$%&\"()*+-./:;<=>?@[\\]^_`{|}~'":
+                content = content.replace(ch, '')
+            if ',' in content:
+                numbers = content.split(',')
+                for num in numbers:
+                    if num.isdigit():
+                        num = int(num)
+                        if num >= 0 and num <= 100:
+                            num_list.append(num)
+            else:
+                if content.isdigit():
+                    content = int(content)
+                    if content >= 0 and content <= 100:
+                        num_list.append(content)
+        return num_list
+    except FileNotFoundError:
+        return num_list
+    except Exception as e:
+        print("AN ERROR OCCURED:", e)
 
 def check_number(num, num_list):
     if num.isdigit():
@@ -119,8 +124,8 @@ def main():
     number_list = []
     number_list = read_file(number_list)
     print("******************************************")# Adding a separation line.
-    number_list = get_number_list(number_list)
-    print(number_list)
+    if len(number_list) < 2:
+        number_list = get_number_list(number_list)
     while True:
         print("1. Get the MEAN.")
         print("2. Get the MEDIAN.")
@@ -161,8 +166,12 @@ def main():
             with open("grades.txt", 'w+') as file:
                 file.write("Grades: ")
             with open("grades.txt", 'a') as file:
-                for num in number_list:
-                    file.write(str(num) + ',')
+                number_list.sort()
+                for index, num in enumerate(number_list):
+                    if index == len(number_list) - 1:
+                        file.write(str(num) + ';')
+                    else:
+                        file.write(str(num) + ',')
             break
         else:
             print(">>>", choice, "is not an option!")
--- test.py
+++ test.py
@@ -1,25 +1,40 @@
 def read(number_list):
-    with open("grades.txt") as file:
-        number = file.read()
-        number = number.lower()
-        for ch in "qwertyuiopasdfghjklzxc v\nbnm!#$%&\"()*+-./:;<=>?@[\\]^_`{|}~'":
-            number = number.replace(ch, '')
-        if ',' in number:
-            numbers = number.split(',')
-            for num in numbers:
-                if num.isdigit():
-                    num = int(num)
-                    if num >= 0 and num <= 100:
-                        number_list.append(num)
+    try:
+        num_list = number_list[:]
+        with open("grades.txt") as file:
+            content = file.read()
+            content = content.lower()
+            for ch in "qwertyuiopasdfghjklzxcv\nbnm! #$%&\"()*+-./:;<=>?@[\\]^_`{|}~'":
+                content = content.replace(ch, '')
+            if ',' in content:
+                numbers = content.split(',')
+                for num in numbers:
+                    if num.isdigit():
+                        num = int(num)
+                        if num >= 0 and num <= 100:
+                            num_list.append(num)
+            else:
+                if content.isdigit():
+                    content = int(content)
+                    if content >= 0 and content <= 100:
+                        num_list.append(content)
+        return num_list
+    except FileNotFoundError: 
+        return num_list
+    except Exception as e:
+        print("AN ERROR OCCURED:", e)
+    
 def main():
     number_list = []
-    read(number_list)
-    number_list.sort()
+    number_list = sorted(read(number_list))
     print(number_list)
     with open("grades.txt", 'w+') as file:
         file.write("Grades: ")
     with open("grades.txt", 'a') as file:
-        for num in number_list:
-            file.write(str(num) + ',')
+        for index, num in enumerate(number_list):
+            if index == len(number_list) - 1:
+                file.write(str(num) + ';')
+            else:
+                file.write(str(num) + ',')
 
 main()
